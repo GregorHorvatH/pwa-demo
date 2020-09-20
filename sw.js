049466b3846f5777021feab09d9fbf13eee5f091
Geri <gregor.horvath83@gmail.com>
2020-09-20 21:24:57 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // registration --> installation --> activation
 // fetch
 
-const staticCacheName = 'static-cache-v1.2';
-const dynamicCacheName = 'dynamic-cache-v1.2';
+const staticCacheName = 'static-cache-v1.3';
+const dynamicCacheName = 'dynamic-cache-v1.3';
 
 const staticAssets = [
   './',
